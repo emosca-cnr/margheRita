ebbd6c40883f4427bdba7c20dee6d8ae0c5055a1
Marynka <72195020+Marynka-MassSpec@users.noreply.github.com>
2020-10-09 12:37:47 +0200

    Update margheRita.R

--- R/margheRita.R
+++ R/margheRita.R
@@ -7,13 +7,21 @@ margheRita <- function(){
 
 
 
-  #preprocessing (Marynka)
+  #preprocessing (Marynka): read input files: data file + metadata
+
+  #filtering:
+  #a)	Delete features that have mass defect 0.4-0.85 (decimals)
+  #b)	Remove features that have Coefficient of Variation in QC > CV than study samples
+  #c)	Select features found in at least 20% samples of at least one group (check with metadata what are observation groups)
 
 
   #normalization
 
 
-  #sample aggregation
+  #sample aggregation - stat x i replicati tecnici
+
+
+  # statistical analysis (check Uni Alberta - metaboanalyst)
 
 
   #outputs
