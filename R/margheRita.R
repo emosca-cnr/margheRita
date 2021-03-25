61422439a95aa7903473666452772059ca8e2ac8
edobelini <76177701+edobelini@users.noreply.github.com>
2021-03-25 16:06:51 +0100

    Update margheRita.R

--- R/margheRita.R
+++ R/margheRita.R
@@ -9,6 +9,13 @@ margheRita <- function(){
 
   #preprocessing (Marynka): read input files: data file + metadata
 
+#read_input_files()
+#PCA_pre_processing()
+#M/Z_filtering()
+#inputation()
+#CV()
+
+
   #filtering:
   #a)	Delete features that have mass defect 0.4-0.85 (decimals)
   #b)	Remove features that have Coefficient of Variation in QC > CV than study samples
@@ -16,13 +23,14 @@ margheRita <- function(){
 
 
   #normalization
+#pqn()
 
 
   #sample aggregation - stat x i replicati tecnici
 
 
   # statistical analysis (check Uni Alberta - metaboanalyst)
-
+#univarite(?)
 
   #outputs
 
