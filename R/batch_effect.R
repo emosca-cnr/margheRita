362ef1401ffa63f589fbd76b65ab3c026de5feec
edobelini <76177701+edobelini@users.noreply.github.com>
2021-04-23 11:07:52 +0200

    Add files via upload

--- R/batch_effect.R
+++ R/batch_effect.R
@@ -1,3 +1,5 @@
+#' @import limma
+#' @export
 #Remove batch effect
 #@param remove batch effect from df (dataframe of data)
 
@@ -5,3 +7,13 @@
 #boxplot(df[4:NCOL(df)], main="Original")
 #df <- removeBatchEffect(df[4:NCOL(df)], batch)
 #boxplot(df[4:NCOL(df)], main="Batch corrected")
+bath_effect <- function(df, columns){
+  box_pre <- boxplot(df[columns[1]:columns[2]], main="Original")
+  df <- removeBatchEffect(df[columns[1]:columns[2]], batch)
+  box_post <- boxplot(df[4:NCOL(df)], main="Batch corrected")
+  box_plot <- box_pre +  box_post
+  return(as.data.frame(df), box_plot)
+}
+
+
+
--- R/m_z_filtering.R
+++ R/m_z_filtering.R
@@ -1,12 +1,12 @@
-#' exclude features with problematic m/z values
 #' @import dplyr
 #' @export
 
 m_z_filtering <- function(df, m_z_average){
   df$quality <- NA
   for(i in 1:nrow(df)) {
-    df$quality[i] <- (df$Average_mz[i] %% 1)
+    df$quality[i] <- (df$m_z_average[i] %% 1)
   }
-  filter(df, df$quality < 0.4 | df > 0.8)
-  plot(density(df$quality))
+  df <- filter(df, df$quality < 0.4 | df > 0.8)
+  qual_plot <- plot(density(df$quality))
+  return(as.data.frame(df), qual_plot)
 }
