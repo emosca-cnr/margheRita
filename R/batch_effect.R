992b579fc20a4d38cdc3ec5681ceda6e0f27699e
edobelini <76177701+edobelini@users.noreply.github.com>
2021-04-23 11:17:02 +0200

    Update batch_effect.R

--- R/batch_effect.R
+++ R/batch_effect.R
@@ -10,7 +10,7 @@
 bath_effect <- function(df, columns){
   box_pre <- boxplot(df[columns[1]:columns[2]], main="Original")
   df <- removeBatchEffect(df[columns[1]:columns[2]], batch)
-  box_post <- boxplot(df[4:NCOL(df)], main="Batch corrected")
+  box_post <- boxplot(df[4:ncol(df)], main="Batch corrected")
   box_plot <- box_pre +  box_post
   return(as.data.frame(df), box_plot)
 }
