33f05512ae74d39b8c09c0b7b173ed640e8b7fb2
emosca-cnr <ettore.mosca@itb.cnr.it>
2021-06-01 18:51:48 +0200

    ettore: minor changes to proper check

--- R/calc_reference.R
+++ R/calc_reference.R
@@ -9,10 +9,10 @@ calc_reference <- function(m_list, sample_col="type", sample_type="QC", approach
   idx_samples <- idx_samples==sample_type
 
   if(approach == "median"){
-   m_list$metab_ann$reference <- apply(m_list$df[, idx_samples], 1, median)
+   m_list$metab_ann$reference <- apply(m_list$data[, idx_samples], 1, median)
   }
   if(approach == "mean"){
-    m_list$metab_ann$reference <- RowMeans(m_list$df[, idx_samples])
+    m_list$metab_ann$reference <- RowMeans(m_list$data[, idx_samples])
   }
 
   return(m_list)
--- R/normalize_profiles.R
+++ R/normalize_profiles.R
@@ -1,5 +1,5 @@
 #' Normalize profiles
-#' @param m_list$df features-by-samples matrix of metabolite levels;
+#' @param m_list$data features-by-samples matrix of metabolite levels;
 #' @param method "log": log2(x+1); "reference": divide each sample by its reference; "pqn": probailistic quotient normalization;
 #' @param reference reference profile;
 #' @export
@@ -7,15 +7,15 @@
 normalize_profiles <- function(m_list, method=c("log", "reference", "pqn")){
 
   if(method == "log"){
-    m_list$df <- log2(m_list$df+1)
+    m_list$data <- log2(m_list$data+1)
   }
 
   if(method == "reference"){
-    m_list$df <- t(t(m_list$df) / m_list$metab_ann$reference)
+    m_list$data <- t(t(m_list$data) / m_list$metab_ann$reference)
   }
 
   if(method == "pqn"){
-    m_list$df <- apply(m_list$df, 2, function(x) pqn(x, xref = m_list$metab_ann$reference)$y)
+    m_list$data <- apply(m_list$data, 2, function(x) pqn(x, xref = m_list$metab_ann$reference)$y)
   }
 
   return(m_list)
