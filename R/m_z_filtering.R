f747c94b6447eeebf0d9e432e2b5fc74eabefba8
edobelini <edobellini92@gmail.com>
2021-08-12 10:59:27 +0200

    Ettore ho sistemato la funzione m_z_filtering specificando due parametri lower_quality_mass_acc e upper_quality_mass_acc

--- R/m_z_filtering.R
+++ R/m_z_filtering.R
@@ -1,19 +1,11 @@
 #' m_z_filtering
 #' @export
 
-m_z_filtering <- function(m_list, do_plot=TRUE){
-
-  #m_list$quality <- numeric(nrow(m_list$df))
-  #for(i in 1:nrow(m_list$df)) {
-  #  m_list$quality[i] <- (m_list$df$Average_mz[i] %% 1)
-  #}
+m_z_filtering <- function(m_list, do_plot=TRUE, lower_quality_mass_acc, upper_quality_mass_acc){
 
   m_list$metab_ann$quality <- m_list$metab_ann$mz %% 1
-  if(do_plot){
-    plot(density(m_list$metab_ann$quality))
-  }
 
-  idx_keep <- m_list$metab_ann$quality < 0.4 | m_list$metab_ann$quality > 0.8
+  idx_keep <- m_list$metab_ann$quality < lower_quality_mass_acc | m_list$metab_ann$quality > upper_quality_mass_acc
 
   cat("Metabolites with appropriate m/z values\n")
   print(table(idx_keep))
@@ -23,6 +15,9 @@ m_z_filtering <- function(m_list, do_plot=TRUE){
 
   m_list$QC <- m_list$QC[idx_keep, ]
 
+   if(do_plot){
+    plot(density(m_list$metab_ann$quality))
+  }
 
   return(m_list)
 
