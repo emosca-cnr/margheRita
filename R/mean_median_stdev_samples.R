24295a7e75af2880932d2007f142378440e7feb6
edobelini <edobellini92@gmail.com>
2021-08-23 14:12:52 +0200

    Ora abbiamo i Fold Change

--- R/mean_median_stdev_samples.R
+++ R/mean_median_stdev_samples.R
@@ -17,12 +17,18 @@ mean_media_stdev_samples<-function(m_list,dirout){
   mean_sample<-mean_sample[,-1]
   mean_sample<-t(mean_sample)
   mean_sample<-as.data.frame(mean_sample)
+  for (i in 1:length(names(mean_sample))) {
+     names(mean_sample)[i] <- paste(names(mean_sample)[i],"mean", sep = "_") 
+  }
 
   median_sample<-stats::aggregate.data.frame(t(m_list$data), list(m_list$sample_ann$class), median)
   rownames(median_sample)<-median_sample$Group.1
   median_sample<-median_sample[,-1]
   median_sample<-t(median_sample)
   median_sample<-as.data.frame(median_sample)
+  for (i in 1:length(names(median_sample))) {
+     names(median_sample)[i] <- paste(names(median_sample)[i],"median", sep = "_") 
+  }
 
 
   sd_sample<-stats::aggregate.data.frame(t(m_list$data), list(m_list$sample_ann$class), sd)
@@ -30,10 +36,19 @@ mean_media_stdev_samples<-function(m_list,dirout){
   sd_sample<-sd_sample[,-1]
   sd_sample<-t(sd_sample)
   sd_sample<-as.data.frame(sd_sample)
+  for (i in 1:length(names(sd_sample))) {
+     names(sd_sample)[i] <- paste(names(sd_sample)[i],"sd", sep = "_") 
+  }
 
 
 
-  Mean_median_stdev<-cbind(mean_sample,median_sample, sd_sample )
+  Mean_median_stdev<-cbind(mean_sample,median_sample, sd_sample)
+  m_list$metab_ann <- cbind(m_list$metab_ann, Mean_median_stdev)
+
+  names(m_list$metab_ann) <- gsub("1", "mean", names(m_list$metab_ann))
+  names(m_list$metab_ann) <- gsub("2", "median", names(m_list$metab_ann))
+
+
   All= paste(dirout, "/mean_median_stdev.csv", sep ="")
   utils::write.csv(Mean_median_stdev, All)
   media= paste(dirout, "/mean.csv", sep ="")
