cef0eb91f9b1fff59a95e278518bf95f848195b5
edobelini <76177701+edobelini@users.noreply.github.com>
2021-08-29 21:01:35 +0200

    ho fatto la vignette

--- R/heatscatter_chromatography.R
+++ R/heatscatter_chromatography.R
@@ -3,19 +3,18 @@
 #' @importFrom grDevices dev.off pdf
 #' @export
 
-heatscatter_chromatography <- function(m_list, mz_limits, rt_limits, stat, color_palette, title) {
-   if (mz_limits==NULL | rt_limits==NULL) {
-      rt <- m_list$metab_ann$rt
-      mz <- m_list$metab_ann$mz
-   } else {
-       rt <- m_list$metab_ann$rt[m_list$metab_ann$rt > rt_limits[1]  & m_list$metab_ann$rt < rt_limits[2]]
-       mz <- m_list$metab_ann$mz[m_list$metab_ann$mz > mz_limits[1]  & m_list$metab_ann$mz < mz_limits[2]]
+heatscatter_chromatography <- function(m_list, mz_limits=NULL, rt_limits=NULL, stat=F, color_palette=1, title=NULL) {
+   df <- as.data.frame(m_list$metab_ann)[2:3]
+   df[is.na(df),] <- 0
+   if (mz_limits!=NULL | rt_limits!=NULL) {
+      df <- df[df$rt >= rt_limits[1] &  df$rt <= rt_limits[2],]
+      df <- df[df$mz >= mz_limits[1] &  df$mz <= mz_limits[2],]
    }
-   heatscatter(m_list$metab_ann$rt, m_list$metab_ann$mz, 
+   heatscatter(df$rt, df$mz, 
       xlab="Retenction Time",
       ylab="m/z feature",
       simulate=ifelse(color_palette==1, F, T),
       main=ifelse(title==NULL, NULL, title),
       add.contour=ifelse(stat==F, F, T))
-return(m_list)
+      return(m_list)
 }
