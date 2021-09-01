3c4d9c01e27a4b57d30d1ec98465f548c7a1e715
edobelini <76177701+edobelini@users.noreply.github.com>
2021-09-01 19:19:16 +0200

    sistemato exp

--- R/exp_mz_rt.r
+++ R/exp_mz_rt.r
@@ -2,9 +2,13 @@
 #' @importFrom plotly plot_ly
 #' @export
 
-exp_mz_rt <- function(m_list, sample=c()) {
+exp_mz_rt <- function(m_list, sample=c(), mz_limits=NULL, rt_limits=NULL) {
    df <- m_list$metab_ann[,c("rt", "mz", names(m_list$metab_ann)[grep(paste(sample[1], "_mean", sep = ""), colnames(m_list$metab_ann))], "quality")]
    df[is.na(df)] <- 0
+   if (!is.null(mz_limits) | !is.null(rt_limits)) {
+      df <- df[df$rt >= rt_limits[1] & df$rt <= rt_limits[2],]
+      df <- df[df$mz >= mz_limits[1] & df$mz <= mz_limits[2],]
+   }
    plotly::plot_ly(df, x= ~rt, y=~mz, z= df[,3], type = 'scatter3d',
         marker = list(color = ~quality, colorscale = c('#FFE1A1', '#683531'),  
                       symbol = "circle", 
