d0b1c28da9631453e6abb2d9a079574ad4ba7cc4
edobelini <76177701+edobelini@users.noreply.github.com>
2021-09-03 19:00:19 +0200

    edo sistemato cose

--- R/heatscatter_chromatography.R
+++ R/heatscatter_chromatography.R
@@ -3,10 +3,16 @@
 #' @importFrom grDevices dev.off pdf
 #' @export
 
-heatscatter_chromatography <- function(m_list, mz_limits=NULL, rt_limits=NULL, ...) {
+heatscatter_chromatography <- function(m_list, mz_limits=NULL, rt_limits=NULL, sample=NULL,...) {
+   if (!is.null(sample)) {
+      df <- m_list$metab_ann[, c("rt", "mz", paste(sample[1], "_mean", sep = ""))]
+      df <- df[df[,3]> 0,]
+      df <- df[1:2]
+   }
 
    df <- m_list$metab_ann[, c("rt", "mz")]
    df[is.na(df)] <- 0
+   
 
    if (!is.null(mz_limits) | !is.null(rt_limits)) {
       df <- df[df$rt >= rt_limits[1] &  df$rt <= rt_limits[2], ]
--- R/m_z_filtering.R
+++ R/m_z_filtering.R
@@ -1,7 +1,7 @@
 #' m_z_filtering
 #' @export
 
-m_z_filtering <- function(m_list, do_plot=TRUE, lower_quality_mass_acc=0.4, upper_quality_mass_acc=0.8){
+m_z_filtering <- function(m_list, do_plot=TRUE, lower_quality_mass_acc=0.4, upper_quality_mass_acc=0.8, color="black"){
 
   m_list$metab_ann$quality <- m_list$metab_ann$mz %% 1
 
@@ -16,7 +16,8 @@ m_z_filtering <- function(m_list, do_plot=TRUE, lower_quality_mass_acc=0.4, uppe
   m_list$QC <- m_list$QC[idx_keep, ]
 
    if(do_plot){
-    plot(density(m_list$metab_ann$quality))
+    plot(density(m_list$metab_ann$quality), xlab="m/z Quality", main="Distrubution of m/z Quality")+
+    polygon(density(m_list$metab_ann$quality), col=color, border=color)
   }
 
   return(m_list)
