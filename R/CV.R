56adc63d563a69536de1cd619331f62e495d1800
vmannella <mannella.valeria@hsr.it>
2022-03-01 18:08:24 +0100

    coe varie

--- R/CV.R
+++ R/CV.R
@@ -1,8 +1,9 @@
 #' Calculate Coefficient of variance (CV) for samples and QC (quality control) to assess the repeatibility of the experiment.
 #' Mean, standard deviation and, then, CV as =(stdev-mean)*100, for each metabolite is calculated for QC and samples. If CV QC>CV sample metabolite is deleted.
-#' @param m_list split into df_QC (dataframe of quality control) and df_samples (df of samples)
+#' @param mRList split into df_QC (dataframe of quality control) and df_samples (df of samples)
+#' @param m_list margheRita mRList
 #' @export
-#' @return m_list with only reliable metabolites
+#' @return mRList with only reliable metabolites
 
 
 
@@ -12,14 +13,14 @@ CV <- function(m_list, dirout) {
 
   dir.create(dirout)
 
-  mean_QC <- apply(m_list$QC, MARGIN=1, mean)
-  sd_QC <- apply(m_list$QC, MARGIN = 1, sd)
+  mean_QC <- apply(mRList$QC, MARGIN=1, mean)
+  sd_QC <- apply(mRList$QC, MARGIN = 1, sd)
   CV_QC <- (sd_QC / mean_QC) * 100
   utils::write.csv(CV_QC, file = paste0(dirout,"/CV_QC.csv"))
 
 
-  mean_Samples <- apply(m_list$data, MARGIN=1, mean)
-  sd_Samples <- apply(m_list$data,MARGIN = 1,sd)
+  mean_Samples <- apply(mRList$data, MARGIN=1, mean)
+  sd_Samples <- apply(mRList$data,MARGIN = 1,sd)
   CV_Samples <- (sd_Samples / mean_Samples) * 100
   utils::write.csv(CV_Samples, file = paste0(dirout,"/CV_Sample.csv"))
 
@@ -31,12 +32,12 @@ CV <- function(m_list, dirout) {
   cat("Metabolites with appropriate CV\n")
   print(table(idx_keep))
 
-  m_list$data <- m_list$data[idx_keep, ] #m_list$data cleaned
-  m_list$metab_ann <- m_list$metab_ann[idx_keep, ] #m_list$data cleaned
+  mRList$data <- mRList$data[idx_keep, ] #mRList$data cleaned
+  mRList$metab_ann <- mRList$metab_ann[idx_keep, ] #mRList$data cleaned
 
-  m_list$QC <- m_list$QC[idx_keep, ] #m_list$data cleaned
+  mRList$QC <- mRList$QC[idx_keep, ] #mRList$data cleaned
 
-  return(m_list)
+  return(mRList)
 }
 
 
