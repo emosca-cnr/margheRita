2d0a355c06fab6689803486c3860c4da93f422a7
emosca-cnr <ettore.mosca@itb.cnr.it>
2023-02-17 16:33:06 +0100

    ettore, cleaning

--- R/load_nutrition_dataset.R
+++ R/load_nutrition_dataset.R
@@ -3,14 +3,14 @@
 #' @param normalized dataset with/without normalized samples.#' 
 #' @export
 
-load_nutrition_dataset <- function(column=NULL, normalized=FALSE){
+load_nutrition_dataset <- function(mode=NULL, normalized=FALSE){
 	
-	column <- match.arg(column, c("POS", "NEG"))
+	column <- match.arg(mode, c("POS", "NEG"))
 	
-	cat("Column", column, "\n")
+	cat("mode", mode, "\n")
 	cat("Normalized", normalized, "\n")
 	
-	input_data_file <- paste0("nutrition_", ifelse(normalized, "norm_", ""), column, ".rds")
+	input_data_file <- paste0("nutrition_", ifelse(normalized, "norm_", ""), mode, ".rds")
 	input_data_file <- system.file("extdata", input_data_file, package = "margheRita")
 	
 	mRList <- readRDS(input_data_file)
