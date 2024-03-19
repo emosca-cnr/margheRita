34bf6b45af41c3ba70e99e3e5441229e1d4e21f8
FrigerioGianfranco <157016529+FrigerioGianfranco@users.noreply.github.com>
2024-03-19 16:43:40 +0100

    Messi i nomi nell'asse x in verticale. Comunque se sono nomi lunghi non si vedono in modo completo, ma almeno messi così in verticale mi sembra un po' più chiaro.

--- R/RLA.R
+++ R/RLA.R
@@ -12,8 +12,8 @@
 #' @importFrom graphics abline
 
 RLA <- function(mRList=NULL, include_QC=FALSE, logged=FALSE, robust=TRUE, pal=NULL, col_by="class", ...){
-  
-  
+
+
   if(include_QC){
     X_data <- cbind(mRList$data, mRList$QC)
     X_sample_ann <- rbind(mRList$sample_ann, mRList$QC_ann)
@@ -21,35 +21,35 @@ RLA <- function(mRList=NULL, include_QC=FALSE, logged=FALSE, robust=TRUE, pal=NU
     X_data <- mRList$data
     X_sample_ann <- mRList$sample_ann
   }
-  
+
   col_factor <- as.factor(X_sample_ann[, col_by])
   n <- length(levels(col_factor))
-  
+
   if(is.null(pal) | length(pal) != n){
     pal <- alphabet2(n)
   }
   colors <- pal[as.numeric(col_factor)]
-  
+
   if(!logged){
     X_data <- log2(X_data + 1)
   }
-  
+
   if(robust){
     X_data <- t(apply(X_data, 1, function(y) y - median(y)))
   }else{
     X_data <- t(apply(X_data, 1, function(y) y - mean(y)))
   }
-  
+
   ## add col_by
-  par(mar=c(4, 4, 1, 1))
-  boxplot(X_data, ..., ylab="x - <x>", main="Relative log Abudance", col = colors)
+  par(mar=c(12, 4, 1, 1))
+  boxplot(X_data, ..., ylab="x - <x>", main="Relative log Abudance", col = colors, las = 2)
   abline(h=0, lty=2)
-  
+
   legend("bottomright", legend = levels(col_factor), pch=15, col = pal, cex=0.5, horiz = T)
-  
-  
+
+
   mRList$RLA <- X_data
-  
+
   return(mRList)
-  
+
 }
